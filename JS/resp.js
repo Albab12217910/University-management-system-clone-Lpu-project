ca70316ca21205b34770ade7f4dc1151e52a4505
Albab Khan <119718294+Albab12217910@users.noreply.github.com>
2025-03-19 15:23:44 +0530

    initial commit

--- JS/resp.js
+++ JS/resp.js
@@ -2,6 +2,7 @@ burger=document.querySelector('.burger')
 navbar=document.querySelector('.navbar')
 navlist=document.querySelector('.nav-list')
 rightnav=document.querySelector('.rightnav')
+//update
 
 
 
